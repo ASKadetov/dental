9960cdf1b78dd1d5062551ed8af663765f58ff4b
ASKadetov <kadetov93@gmail.com>
2026-03-22 20:16:41 +0700

    ready version

--- app/app.py
+++ app/app.py
@@ -1,17 +1,34 @@
+from flask import Flask, render_template, request, redirect, flash
 import os
 import requests
-from flask import Flask, render_template, request, redirect, flash
+
 
 app = Flask(__name__)
-app.secret_key = "supersecretkey"
+app.secret_key = "secret"
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-GOOGLE_DOC_URL = "https://drive.google.com/your_doc_link_here"
+GOOGLE_DOC_URL = "https://docs.google.com/document/d/1PL-zdFlLb-MkSX5A8a7n34v3_O7Btsbp/edit?usp=sharing&ouid=115948205042641805739&rtpof=true&sd=true"
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/price")
+def price():
+    return render_template("price.html")
+
+@app.route("/price-file")
+def price_redirect():
+    return redirect(GOOGLE_DOC_URL)
 
 @app.route("/contacts", methods=["GET", "POST"])
 def contacts():
@@ -21,30 +38,18 @@ def contacts():
         phone = request.form.get("phone")
 
         message = f"""
-📩 Новый вопрос с сайта
+📩 Новый вопрос
 
-❓ Вопрос: {question}
-📧 Email: {email}
-📱 Телефон: {phone}
+❓ {question}
+📧 {email}
+📱 {phone}
 """
+        requests.post(
+            f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage",
+            data={"chat_id": TELEGRAM_CHAT_ID, "text": message}
+        )
 
-        send_to_telegram(message)
-        flash("Сообщение отправлено!")
+        flash("Отправлено!")
         return redirect("/contacts")
 
     return render_template("contacts.html")
-
-@app.route("/prices")
-def prices():
-    return redirect(GOOGLE_DOC_URL)
-
-def send_to_telegram(text):
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    data = {
-        "chat_id": TELEGRAM_CHAT_ID,
-        "text": text
-    }
-    requests.post(url, data=data)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
